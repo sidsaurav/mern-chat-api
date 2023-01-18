d94688bd59b92f4576a7a1556c67cbac41fd10ae
Siddharth Saurav <siddharth.saurav.che19@itbhu.ac.in>
2023-01-19 03:29:54 +0530

    cors error fix

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.use(express.json())
 const PORT = process.env.PORT || 3000
 
 connectDB()
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/api/user', userRouter)
 app.use('/api/chat', chatRouter)
 app.use('/api/message', messageRouter)

e41087fd4534f0b9af1a77c0c67ba1f0edd75872
Siddharth Saurav <sid.saurav11@gmail.com>
2023-01-19 04:02:28 +0530

    socket io cors fix

--- server.js
+++ server.js
@@ -56,9 +56,9 @@ const server = app.listen(
 
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
-    // cors: {
-    //     origin: 'http://localhost:3000',
-    // },
+    cors: {
+        origin: '*',
+    },
 })
 
 io.on('connection', (socket) => {
